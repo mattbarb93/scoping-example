0c534fdb7f831a0dd0312f34279d2bd0a403ef5a
Matheus Barbosa <mattbarb93@gmail.com>
2021-08-11 22:32:38 -0400

    more examples

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   //   console.log(firstName); // Can be accessed, since "firstName" is in the global scope
@@ -31,3 +31,103 @@ const firstName = 'Jonas';
 calcAge(1991);
 //console.log(age); // Cannot have access to variables of a child scope.
 //printAge(); //Cannot have access to functions of child scopes
+
+*/
+/*
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1991);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+
+// f();
+
+*/
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     //console.log(this);
+//     //The way to preserve the "this" keyword in blocks (pre ES6) is to create another variable
+//     // const self = this;
+//     // console.log(2037 - this.year);
+
+//     // const isMillenial = function () {
+//     //   console.log(self);
+//     //   console.log(self.year >= 1981 && self.year <= 1996);
+//     // };
+
+//     // isMillenial();
+
+//     //In ES6, you can use the this keyword if you use an arrow function
+//     const isMillenial = () => {
+//       console.log(this);
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//     };
+
+//     isMillenial();
+//   },
+//   greet: function () {
+//     console.log(`Hey ${this.firstName}`);
+//   },
+// };
+
+// jonas.greet();
+// jonas.calcAge();
+
+//ARGUMENTS KEYWORD
+// const addExpr = function (a, b) {
+//   console.log(arguments);
+//   return a + b;
+// };
+// addExpr(2, 5);
+
+// const addArrow = (a, b) => {
+//   console.log(arguments);
+//   return a + b;
+// };
+
+// addArrow(2, 5, 8);
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me:', me);
